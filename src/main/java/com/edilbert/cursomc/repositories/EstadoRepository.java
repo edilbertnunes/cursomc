9ee080d39bf4bf7142a6ea5f4f6c69ebae0de920
Edilbert Nunes <edilbert.nunes@gmail.com>
2023-04-27 15:09:25 -0300

    Aula 55 - Cidade e Estado

---
+++ src/main/java/com/edilbert/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.edilbert.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edilbert.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
