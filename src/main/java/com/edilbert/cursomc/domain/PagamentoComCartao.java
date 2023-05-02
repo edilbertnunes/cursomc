146df4ae667d21d7ad3db32c3d9b3f0a4073e99e
Edilbert Nunes <edilbert.nunes@gmail.com>
2023-05-02 11:38:34 -0300

    Aula 60 - Pedido, EstadoPagamento e Pagamento - Parte 3

--- src/main/java/com/edilbert/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/edilbert/cursomc/domain/PagamentoComCartao.java
@@ -1,7 +1,10 @@
 package com.edilbert.cursomc.domain;
 
+import javax.persistence.Entity;
+
 import com.edilbert.cursomc.domain.enums.EstadoPagamento;
 
+@Entity
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/edilbert/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/edilbert/cursomc/repositories/PedidoRepository.java
@@ -3,9 +3,9 @@ package com.edilbert.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.edilbert.cursomc.domain.Cliente;
+import com.edilbert.cursomc.domain.Pedido;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
 }
